c70d2b15a55ae7117b0cca4705fb2d65c01a63fb
wangzhe12 <wangzhe12@baidu.com>
2019-01-05 19:25:50 +0800

    fixed bug

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -161,7 +161,7 @@ class TLDetector(object):
         if closest_light:
             state = self.get_light_state(closest_light)
             return line_wp_idx, state
-        self.waypoints = None
+        
         return -1, TrafficLight.UNKNOWN
 
 if __name__ == '__main__':
